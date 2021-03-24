d01aec4d8d725a02d784ab4852bf5d20447fc54b
赖婷婷 <8211517+lai-tingting@user.noreply.gitee.com>
2021-03-24 22:37:20 +0800

    鼠标移入移出功能

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,16 +2,31 @@ import React, { Component } from 'react'
 //引入样式文件
 import './index.css';
 export default class Item extends Component {
+    //数据状态
+    state = {
+        mouse:false
+    }
     render() {
         const {name} = this.props;
+        const {mouse} = this.state;
         return (
-            <li>
+            <li style={{backgroundColor: mouse ? '#D0F2E7' : "#fff" }} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
                 <label>
                   <input type="checkbox"/>
                   <span>{name}</span>
                 </label>
-                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                <button className="btn btn-danger" style={{display: mouse ? 'block' : 'none'}}>删除</button>
              </li>
         )
     }
+    //鼠标移入移出事件
+    handleMouse = (flag) => {
+        return () => {
+            this.setState({
+                mouse:flag
+            })
+            
+        }
+    }
+    
 }
